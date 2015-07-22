fc714bfb631d41912251cf3898ee4ca2646ec23b
christianrober <christianrober@gmail.com>
2015-07-22 12:40:40 -0400

    Added new license header.

--- audit.cpp
+++ audit.cpp
@@ -1,6 +1,25 @@
-/**
- *    Copyright (C) 2014 Tokutek Inc.
- */
+/* -*- mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+// vim: ft=cpp:expandtab:ts=8:sw=4:softtabstop=4:
+
+/*======
+This file is part of Percona Server for MongoDB.
+
+Copyright (c) 2006, 2015, Percona and/or its affiliates. All rights reserved.
+
+    Percona Server for MongoDB is free software: you can redistribute
+    it and/or modify it under the terms of the GNU Affero General
+    Public License, version 3, as published by the Free Software
+    Foundation.
+
+    Percona Server for MongoDB is distributed in the hope that it will
+    be useful, but WITHOUT ANY WARRANTY; without even the implied
+    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+    See the GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public
+    License along with Percona Server for MongoDB.  If not, see
+    <http://www.gnu.org/licenses/>.
+======= */
 
 #include "mongo/pch.h"
 
